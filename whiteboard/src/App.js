d65946102b2ec00b5a45b7f3d453a1e74f70d6a7
Dishakhabiya <dishaj726@gmail.com>
2026-06-21 17:56:45 +0530

    Backup before migrating to Vite

--- whiteboard/src/App.js
+++ whiteboard/src/App.js
@@ -1,8 +1,14 @@
 import Board from "./Components/Board";
+import ToolBar from "./Components/ToolBar";
 function App() {
 
   return (
-   <Board />
+    <>
+      <h1 className="text-3xl font-bold text-red-500">Hello</h1>
+       <Board />
+       <ToolBar />
+    </>
+
     
 
   );
--- whiteboard/src/Components/ToolBar/index.js
+++ whiteboard/src/Components/ToolBar/index.js
@@ -1,8 +1,12 @@
 import React from 'react'
-
+import classes from "./index.module.css";
 const ToolBar = () => {
   return (
-    <div>ToolBar</div>
+    <div className={classes.container} >
+      <div className={classes.toolItem}>
+        
+      </div>
+    </div>
   )
 }
 
